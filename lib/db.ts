60ae5d62cfb5c722fb3d1bfb1a39c623f66482d9
Carlos Fontes <fonstack@gmail.com>
2024-04-13 15:37:04 +0100

    added dbName

--- lib/db.ts
+++ lib/db.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectMongoDb = async () => {
   try {
     if (!process.env.MONGODB_URI) throw new Error("MONGODB_URI is not defined");
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, { dbName: "ynab-syncer" });
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log("Error connecting to MongoDB: ", error);
